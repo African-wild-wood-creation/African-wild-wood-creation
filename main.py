de87c27cdd231dcd3ccb8107e3f498fdaf83613b
dwaynxz <dwayne604012@gmail.com>
2025-12-28 21:49:23 +0000

    integrate database to main

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
-
+from extensions import db
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydb.db" # initialise sql alchemy databse path
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+db.init_app(app)
+
+from models import ProductCategories, Products
 
 @app.route("/")
 def home():
@@ -13,11 +18,18 @@ def contact():
 
 @app.route("/products")
 def products():
-    return render_template("products.html")
+    product_categories = ProductCategories.query.all()
+    return render_template("products.html", categories=product_categories)
 
 @app.route("/products-temp")
 def products_temp():
     return render_template("test.html")
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        categories = ["tables", "clocks", "mirrors", "lights", "shelves", "coat hangers", "support legs", "signs", "trays", "wine racks", "wood"]
+        for category in categories:
+            db.session.add(ProductCategories(name=category, img_url=f"{category}_category.png"))
+        db.session.commit() # creates databse
     app.run(debug=True)
