8718b8febabbc7461e906e369c7c67b5e8b16d4b
dwaynxz <dwayne604012@gmail.com>
2025-12-28 00:54:37 +0000

    add float datatype for price column

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from extensions import db
+from sqlalchemy import Numeric
 
 class ProductCategories(db.Model):
     __tablename__ = "product_categories"
@@ -14,4 +15,5 @@ class Products(db.Model):
     product_id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
     name = db.Column(db.String(100), nullable=False)
     img_url = db.Column(db.String(200), nullable=False)
+    price = db.Column(db.Numeric(10,2), nullable=False)
     category_id = db.Column(db.Integer, db.ForeignKey("product_categories.category_id"), nullable=False)
