fc20f168e982cf4add66957703bbbdf3addefa9d
dwaynxz <dwayne604012@gmail.com>
2025-12-29 17:43:23 +0000

    fix dublicate databse elements created

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from extensions import db
+import os
+
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydb.db" # initialise sql alchemy databse path
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -26,10 +28,11 @@ def products_temp():
     return render_template("test.html")
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-        categories = ["tables", "clocks", "mirrors", "lights", "shelves", "coat hangers", "support legs", "signs", "trays", "wine racks", "wood"]
-        for category in categories:
-            db.session.add(ProductCategories(name=category, img_url=f"{category}_category.png"))
-        db.session.commit() # creates databse
+    if os.environ.get("WERKZEUG_RUN_MAIN") == "true": # creates database only in the main program
+        with app.app_context():
+            db.create_all()
+            categories = ["tables", "clocks", "mirrors", "lights", "shelves", "coat hangers", "support legs", "signs", "trays", "wine racks", "wood"]
+            for category in categories:
+                db.session.add(ProductCategories(name=category, img_url=f"{category}_category.png"))
+            db.session.commit() # creates databse
     app.run(debug=True)
